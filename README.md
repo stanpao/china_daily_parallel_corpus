fe93d19e57749209a154e2101dcf44898c2bfc3c
Stan Pao <stanpao@outlook.com>
2021-01-22 17:58:01 +0800

    check

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 en-zh 平行语料库以及对应异步爬虫工具
 
 ### environ
-Python  3.8.3
-asyncio 3.4.3
+- Python    3.8.3
+- asyncio   3.4.3
+- aiohttp   3.7.2
 
 ### run
 python china_daily_bilingual.py
--- china_daily_bilingual.py
+++ china_daily_bilingual.py
@@ -45,7 +45,7 @@ async def obtain_content(session, url):
     log.info(f'{time.asctime()} Parsing {url} is done.')
     return parse_parallel_p(sentences)
 
-async def main(file):
+async def bilingual_news(file):
     num = 0
     with open(file, 'w', encoding='utf8') as f:
         while True:
@@ -63,11 +63,65 @@ async def main(file):
                         for en, zh in result:
                             f.write(f'{en}\t{zh}\n')
                 except ValueError:
-                    pass
+                    log.info(result)
 
+async def obtain_pureen_content(session, url):
+    def parse_sentence(sentences):
+        sentences = [i for i in sentences if not re.match('^\s$', i)]
+        result = list()
+        for i in sentences:
+            if re.search('[a-zA-Z\u4e00-\u9fa5]', i):
+                if re.search('[‘“【（]?[\u4e00-\u9fa5]', i):
+                    idx = re.search('[‘“【（]?[\u4e00-\u9fa5]', i).span()[0]
+                    en, zh = i[:idx], i[idx:]
+                    if len(en) > 20 and len(zh) > 20:
+                        result.append((en.strip(), zh.strip()))
+            else:
+                continue
+
+        return result
+
+    try:
+        resp = await session.get(url)
+        content = BeautifulSoup(await resp.text(encoding='utf8'), features='lxml')
+        sentences = [i.text for i in content.select('#Content p')]
+        log.info(f'{time.asctime()} Parsing {url} is done.')
+        return parse_sentence(sentences)
+    except aiohttp.client_exceptions.ClientResponseError:
+        return []
+
+
+async def bilingual_book(file):
+    num = 0
+    with open(file, 'w', encoding='utf8') as f:
+        while True:
+            async with aiohttp.client.ClientSession(raise_for_status=True) as session:
+                num += 1
+                if num == 1:
+                    url = 'http://language.chinadaily.com.cn/pureenglish.html'
+                else:
+                    url = f'http://language.chinadaily.com.cn/pureenglish_{num}.html'
+                try:
+                    resp = await session.get(url)
+                except aiohttp.client_exceptions.ClientResponseError:
+                    break
+
+                item_urls = BeautifulSoup(await resp.text(encoding='utf8'), features='lxml')
+                item_urls = [item['href'] for item in item_urls.select('.busBox1 h3 a')]
+                if not item_urls:
+                    break
+                data = await asyncio.gather(*[obtain_pureen_content(session, 'http://language.chinadaily.com.cn/' + uri)
+                                              for uri in item_urls])
+                try:
+                    for result in data:
+                        for en, zh in result:
+                            f.write(f'{en}\t{zh}\n')
+                except ValueError:
+                    log.info(result)
 
 if __name__ == '__main__':
-    asyncio.run(main('china_daily_bilingual.tsv'))
+    # asyncio.run(bilingual_news('china_daily_bilingual.tsv'))
+    asyncio.run(bilingual_book('china_daily_pure.tsv'))
 
     # Below is for test
     # async def test():
